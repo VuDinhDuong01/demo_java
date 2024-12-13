8c281e18edc510b2f4a4220798651d139816d6d1
duong.vd <duong.vd@ptf.com.vn>
2024-12-13 15:19:18 +0700

    config oauth2

--- src/main/java/com/example/demo/controller/BranchController.java
+++ src/main/java/com/example/demo/controller/BranchController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/com/example/demo/controller/ProducController.java
+++ src/main/java/com/example/demo/controller/ProducController.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
