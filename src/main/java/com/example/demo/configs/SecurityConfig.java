51812053bc1b9f287e2f893c35879b207cda1e4a
duong.vd <duong.vd@ptf.com.vn>
2024-12-17 15:37:34 +0700

    resolve conflict

--- src/main/java/com/example/demo/configs/SecurityConfig.java
+++ src/main/java/com/example/demo/configs/SecurityConfig.java
@@ -57,9 +57,7 @@ public class SecurityConfig {
                                 .authorizeHttpRequests((authorize) -> authorize
                                                 .requestMatchers(HttpMethod.POST, PUBLIC_ROUTE).permitAll()
                                                 .requestMatchers(HttpMethod.PUT, "/api/v1/*").permitAll()
-                                                .requestMatchers("/login", "/oauth2/**", "/api/v1/home",
-                                                                "/custom-callback/**")
-                                                .permitAll()
+                                                .requestMatchers("/login", "/oauth2/**", "/api/v1/home","/custom-callback/**").permitAll()
                                                 .requestMatchers(HttpMethod.GET, PUBLIC_ROUTER_SWAGGER).permitAll()
                                                 // .requestMatchers("/api/v1/oauth/**").permitAll()
                                                 .anyRequest().authenticated())
@@ -122,5 +120,4 @@ public class SecurityConfig {
         public XSSFSheet sheet(XSSFWorkbook workbook) {
                 return workbook.createSheet("User");
         }
-
 }
